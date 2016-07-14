b9ad13b4f23dd311b34d8ac0185efd3f77772e18
mikevh <mikevh@gmail.com>
2016-07-14 07:53:58 -0700

    File new project > aspnet mvc

---
+++ Message.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Message.Web.MvcApplication" Language="C#" %>
---
+++ Message.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Message.Web.Startup))]
+namespace Message.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
