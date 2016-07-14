5a72cd7fbbe3aec72bc76bb3a2f5c5b8ec551d69
mikevh <mikevh@gmail.com>
2016-07-14 08:58:38 -0700

    WPF chat client

--- Message.WPF/MainWindow.xaml.cs
+++ Message.WPF/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.AspNet.SignalR.Client;
+using Microsoft.AspNet.SignalR.Client.Hubs;
 
 namespace Message.WPF
 {
@@ -20,9 +23,55 @@ namespace Message.WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        public HubConnection Connection { get; set; }
+        public IHubProxy HubProxy { get; set; }
+        const string ServerURI = "http://localhost:16845";
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            ConnectAsync();
+        }
+
+        private async void ConnectAsync()
+        {
+            Connection = new HubConnection(ServerURI);
+            Connection.Closed += Connection_Closed;
+            HubProxy = Connection.CreateHubProxy("ChatHub");
+            
+            //Handle incoming event from server: use Invoke to write to console from SignalR's thread
+            HubProxy.On<string, string>("broadcastMessage", (name, message) =>
+                this.Dispatcher.Invoke(() =>richTextBox.AppendText($"{name}: {message}\r"))
+            );
+
+            try
+            {
+                await Connection.Start();
+            }
+            catch (HttpRequestException ex)
+            {
+                richTextBox.AppendText("Unable to connect to server: Start server before connecting clients:\n" + ex.Message + "\n\n");
+                return;
+            }
+
+            //Show chat UI; hide login UI
+            richTextBox.AppendText("Connected to server at " + ServerURI + "\r");
+        }
+
+        void Connection_Closed()
+        {
+            //Hide chat UI; show login UI
+            var dispatcher = Application.Current.Dispatcher;
+            dispatcher.Invoke(() => richTextBox.AppendText("Connection closed\n"));
+        }
+
+        private void sendButton_Click(object sender, RoutedEventArgs e)
+        {
+            HubProxy.Invoke("Send", nameTextBox.Text, messageTextBox.Text);
         }
     }
 }
